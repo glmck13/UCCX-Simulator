33546020442fc07262f87a0b59b246b01a5ab3f2
glmck13 <glmck13@verizon.net>
2022-09-20 12:46:36 -0400

    Update sim.sh

--- WWW/cgi-bin/sim.sh
+++ WWW/cgi-bin/sim.sh
@@ -17,7 +17,7 @@ case "$cmd" in
 	voice)
 		[ ! "$arg1" ] && arg1="Nothing%20to%20say"
 		[ ! "$arg2" ] && arg2="out$$.wav"
-		cscript /NoLogo speech.js "$arg1" "$arg2"
+		cscript /NoLogo speak.js "$arg1" "$arg2"
 		mv $arg2 ../cdn
 		vxml "$arg2"
 		;;
